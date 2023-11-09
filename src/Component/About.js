147af914f2e2b34fed2f819a2db73909a2c01239
SHARIQADNAN <adnanshariq333@gmail.com>
2023-11-09 22:37:50 +0530

    first commit

---
+++ src/Component/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section className="about">
+            <h2>About Me</h2>
+            <p>Hello! I'm SANMATHI PRIYA K R, a passionate and versatile full stack developer with a love for creating innovative web solutions.</p>
+        </section>
+    );
+}
+
+export default About;
---
+++ src/Component/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <section className="home">
+            <h1>SANMATHI PRIYA K R</h1>
+            <h1>Welcome to My Portfolio</h1>
+            <p>Full stack Developer | Node js</p>
+        </section>
+    );
+}
+
+export default Home;
