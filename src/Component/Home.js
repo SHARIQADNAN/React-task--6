8bfb2a1293d137c504297c0cca49db2a730895bc
SHARIQADNAN <101619385+SHARIQADNAN@users.noreply.github.com>
2023-11-09 23:15:17 +0530

    Update Home.js

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
     return (
         <section className="home">
-            <h1>SANMATHI PRIYA K R</h1>
+            <h1>Shariq Adnan</h1>
             <h1>Welcome to My Portfolio</h1>
             <p>Full stack Developer | Node js</p>
         </section>
